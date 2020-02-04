488dd82351b16fa6b02a56d06bbaf5687a98c308
Markus <Ederl>
2020-02-04 16:21:22 +0100

    Updates

---
+++ precache-manifest.f2c6d4c3f94d208bf9c2d3bf662ad26f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6faca5a108dcf87f544584d1f88d2f95",
+    "url": "/JS_REACT_ReduxExample01/index.html"
+  },
+  {
+    "revision": "f9498a4578861eb937a1",
+    "url": "/JS_REACT_ReduxExample01/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "c3b626c29f6146af7994",
+    "url": "/JS_REACT_ReduxExample01/static/css/main.eaeac05c.chunk.css"
+  },
+  {
+    "revision": "f9498a4578861eb937a1",
+    "url": "/JS_REACT_ReduxExample01/static/js/2.aa54faf7.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/JS_REACT_ReduxExample01/static/js/2.aa54faf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3b626c29f6146af7994",
+    "url": "/JS_REACT_ReduxExample01/static/js/main.2d7ad733.chunk.js"
+  },
+  {
+    "revision": "04324671a694262d1901",
+    "url": "/JS_REACT_ReduxExample01/static/js/runtime-main.021ed2ba.js"
+  }
+]);
